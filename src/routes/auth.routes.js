c52d2a8b958a4c6ea842d641360805504ec4e9b4
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-04-28 12:23:55 -0500

    verify token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,5 +7,5 @@ const authController = new AuthController();
 authRoutes.post('/register', authController.Register);
 authRoutes.post('/login', authController.Login);
 authRoutes.post('/logout', authController.Logout);
-
+authRoutes.get('/verify', authController.verifyToken)
 export default authRoutes;
